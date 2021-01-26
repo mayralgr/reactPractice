2fe2ff90f96d5853c0eb67542e117f95fa8be42a
mayralgr <ml17ft@gmail.com>
2021-01-25 23:14:42 -0600

    Update before separete use states

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  
-    /**
-    State just exist in components
-   */
-  state = {
+const app = props => {
+  const [ personState, setPersonsState ] = useState({
     persons: [
       { name: 'Max', age: 28 }, 
       { name: 'Manu', age: 29 }, 
       { name: 'Stephanie', age: 26 }, 
     ],
     otherStat: 'some othe values'
-  }
+  });
+  
 
-  switchEventHandler = () => {
+  const switchEventHandler = () => {
     // console.log('WAS CLICK');
-    // Dont do this this.state.persons[0].name = 'Maximilian';
-    this.setState({
+    // Dont do this personState.persons[0].name = 'Maximilian';
+
+    // does not merge the object or array
+    setPersonsState({
       persons: [
         { name: 'Maximilian', age: 28 }, 
         { name: 'Manu', age: 29 }, 
         { name: 'Stephanie', age: 27 }, 
       ],
+      otherStat: persons.otherStat,
     })
   }
-  render() {
+  //return() {
     return (
       <div className="App">
         <h1>Hi I am a react app </h1>
-        <button onClick={this.switchEventHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>More things</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchEventHandler}>Switch Name</button>
+        <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+        <Person name={personState.persons[1].name} age={personState.persons[1].age}>More things</Person>
+        <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
       </div>
     );
-  }
+  //}
 }
 
-export default App;
+export default app;
