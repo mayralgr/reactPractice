a20da20a195cfa1d55c46927fa2a2193ae2d97f0
mayralgr <ml17ft@gmail.com>
2021-01-25 23:03:17 -0600

    Update before functional components with hooks

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -13,13 +13,26 @@ class App extends Component {
       { name: 'Max', age: 28 }, 
       { name: 'Manu', age: 29 }, 
       { name: 'Stephanie', age: 26 }, 
-    ]
+    ],
+    otherStat: 'some othe values'
+  }
+
+  switchEventHandler = () => {
+    // console.log('WAS CLICK');
+    // Dont do this this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 }, 
+        { name: 'Manu', age: 29 }, 
+        { name: 'Stephanie', age: 27 }, 
+      ],
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>Hi I am a react app </h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchEventHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>More things</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
