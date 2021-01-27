0af37a2adba56432622d58e5241b93ab6b1287fd
mayralgr <ml17ft@gmail.com>
2021-01-26 20:40:09 -0600

    Add two binding, pass of methods

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,45 +1,59 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [ personState, setPersonsState ] = useState({
+class App extends Component {
+  state = {	
     persons: [
       { name: 'Max', age: 28 }, 
       { name: 'Manu', age: 29 }, 
       { name: 'Stephanie', age: 26 }, 
     ],
-  });
+  }
 
-  const [ otherState, setOtheState ] = useState('some other value');
-
-  console.log(personState, otherState);
-
-  const switchEventHandler = () => {
+  switchEventHandler = ( newName ) => {
     // console.log('WAS CLICK');
     // Dont do this personState.persons[0].name = 'Maximilian';
 
-    // does not merge the object or array
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 }, 
+        { name: newName, age: 28 }, 
         { name: 'Manu', age: 29 }, 
         { name: 'Stephanie', age: 27 }, 
       ],
     })
   }
-  //return() {
+
+  nameChangeHandler= ( event ) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 }, 
+        { name: event.target.value, age: 29 }, 
+        { name: 'Stephanie', age: 26 }, 
+      ],
+    })
+  }
+  // better to use bind than arrow functions
+  render() {
     return (
       <div className="App">
         <h1>Hi I am a react app </h1>
-        <button onClick={switchEventHandler}>Switch Name</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-        <Person name={personState.persons[1].name} age={personState.persons[1].age}>More things</Person>
-        <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+        <button onClick={() => this.switchEventHandler('MAXIMILIAN')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchEventHandler.bind(this,'Maxim!!!')}
+        changed={this.nameChangeHandler}
+        >More things</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
-  //}
+  }
 }
 
-export default app;
+export default App;
