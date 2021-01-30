a1c3ff021227ce1a89c60973ab57b10a4c4f216b
mayralgr <ml17ft@gmail.com>
2021-01-30 12:24:15 -0600

    Add dynamic styling

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -56,7 +56,8 @@ class App extends Component {
   render() {
     // inline styles
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color:'white',
       font:'inherit',
       border: '1x solid blue',
       padding: '8px',
@@ -80,11 +81,23 @@ class App extends Component {
 
           </div>
       );
+      style.backgroundColor = 'red';
 
     }
+
+    const classes = [];
+
+    if ( this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if ( this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi I am a react app </h1>
+        <p className={classes.join(' ')}>This is really working</p>
         <button style={style}
         onClick={this.tooglePersonsHandler}>Toogle persons</button>
         { persons }
