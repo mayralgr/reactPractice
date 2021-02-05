0aa9038371981c248a1951bd3b05df59611c3cad
mayralgr <ml17ft@gmail.com>
2021-02-04 23:03:15 -0600

    Add useEffect hook

--- react-application/src/components/Cockpit/Cockpit.js
+++ react-application/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const Cockpit = (props) => {
+    useEffect(() => {
+        console.log('cockpit useEffect');
+        // http 
+        setTimeout(() => {
+            alert('save data to cloud');
+        }, 1000);
+    }, [props.persons]) // point to the data that is use in use effect
+    // if passes [] it runs everytime a dependency changes, if no dependency, just runs once
     const assignedClasses = [];
     let btnClass = '';
     
