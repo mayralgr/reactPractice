22c5293830c26d1e1ada75aeb36d2c81751d5915
mayralgr <ml17ft@gmail.com>
2021-02-10 22:09:40 -0600

    Add HOCs withClass function wrapping

--- react-application/src/containers/App.js
+++ react-application/src/containers/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import classes from'./App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
-
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Aux';
 class App extends Component {
 
   constructor(props) {
@@ -97,7 +97,7 @@ class App extends Component {
     } 
 
     return (
-        <WithClass classes={classes.App}>
+        <Aux>
           <button onClick={()=> {
             this.setState({showCockpit: false})
           }} > Remove cockpit</button>
@@ -109,13 +109,13 @@ class App extends Component {
           />
           : null}
           { persons }
-        </WithClass>
+        </Aux>
       
     );
   }
 }
 
-export default App;
+export default withClass(App,classes.App);
 
 //export default Radium(App);
 
